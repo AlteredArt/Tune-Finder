5d2e9eb66f835534b2e8b88eb81ca95bd23ae0e1
Altered Art <cherokeedreemer@gmail.com>
2020-09-21 15:24:07 -0600

    work on styling and adding features

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Artist from './Component/Artist/Artist';
+import Tracks from './Component/Tracks/Track';
+import Search from './Component/Search/Search'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API = 'https://spotify-api-wrapper.appspot.com'
+class App extends React.Component {
+
+  state = {
+    artist: null,
+    tracks: []
+  }
+
+  searchArtist = artistQuery => {
+    fetch(`${API}/artist/${artistQuery}`)
+      .then(response => response.json())
+      .then(json => {
+        if (json.artists.total > 0) {
+          const artist = json.artists.items[0];
+          this.setState({ artist })
+          fetch(`${API}/artist/${artist.id}/top-tracks`)
+            .then(response => response.json())
+            .then(json => this.setState({ tracks: json.tracks }))
+            .catch(error => alert(error.message))
+        }
+      })
+      .catch(error => alert(error.message));
+  }
+
+
+  render() {
+    return (
+      <div className="App" style={{ "textAlign": "center" }}>
+        <h2>Tune Finder</h2>
+        <Search searchArtist={this.searchArtist} />
+        <Artist artist={this.state.artist} />
+        <Tracks tracks={this.state.tracks} />
+      </div >
+    );
+  }
+
 }
 
 export default App;
